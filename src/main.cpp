ee600d530e3dc33e2e23bae3ab5440ce6ee10721
frozar <fabien.rozar@gmail.com>
2022-05-07 11:41:28 +0400

    wip

--- src/main.cpp
+++ src/main.cpp
@@ -15,6 +15,7 @@
 #include <AiEsp32RotaryEncoder.h>
 #include <Wire.h>
 #include <Keypad_I2C.h>
+#include <MAX1704X.h>
 
 #include "commandClavier.h"
 #include "boutonMach3.h"
@@ -23,6 +24,22 @@
 #include "screen.h"
 #include "sleep.h"
 #include "setup_i2c.h"
+
+// ***
+// *** Connections:
+// *** 1) Conect SDA pin to A4 on Uno
+// *** 2) Connect SCL pin to A5 on Uno
+// *** 3) Connect the GND pin to ground on the Uno.
+// ***
+// *** For other devices lookup the correct i2C
+// *** (SDA and SCL) pins.
+// ***
+
+//
+// Define the MAX17403 fuel gauge. Use MAX17044_mV
+// for the MAX17044.
+//
+MAX1704X _fuelGauge = MAX1704X(MAX17043_mV);
 
 /**
  * @brief Bluetooth keyboard variable
@@ -74,6 +91,35 @@ void setup()
 
   screenSendMessage("TLC-M3 ON");
 
+  //
+  // Initialize the fuel gauge.
+  //
+  if (_fuelGauge.begin(true, static_cast<uint8_t>(0x32)))
+  {
+    Serial.println("The MAX1704X device was found.\n");
+
+    //
+    // Reset the device.
+    //
+    Serial.println("Resetting device...");
+    _fuelGauge.reset();
+    delay(250);
+
+    //
+    // Issue a quickstart command and wait
+    // for the device to be ready.
+    //
+    Serial.println("Initiating quickstart mode...");
+    _fuelGauge.quickstart();
+    delay(125);
+  }
+  else
+  {
+    Serial.println("The MAX1704X device was NOT found.\n");
+    while (true)
+      ;
+  }
+
   timestampOrigin = millis();
 }
 
@@ -93,8 +139,11 @@ void loop()
 {
   long elapseTime = millis() - timestampOrigin;
 
-  if (5000 < elapseTime)
+  if (1000 < elapseTime)
   {
+    Serial.print("Battery Percent is ");
+    Serial.print(_fuelGauge.percent(true));
+    Serial.println("%");
     checkBluetoothIsConnected();
     timestampOrigin = millis();
   }
